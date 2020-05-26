5de9e7d37c661911d9ee6998cecb52520feefed5
Zhangmingyang Su <60785877+Zhangmingyang-Su@users.noreply.github.com>
2020-05-25 20:51:05 -0400

    Update Linkedlist.py

--- Linkedlist.py
+++ Linkedlist.py
@@ -1,1 +1,18 @@
+# reverse LinkedList(iterative)
+def reverseLinkedList(head):
+  prev = None
+  while head:
+    next_node = head.next
+    head.next = prev
+    prev = head
+    head = next_node
+  return prev
 
+# reverse LinkedList(recursive)
+def reverseLinkList(head):
+  if head is None or head.next is None:
+    return head
+  next_node = reverseLinkList(head.next)
+  head.next.next = head
+  head.next = None
+return next_head
